5193fb3dffd0bc87b0930a8853527758f14ba8b0
Daniel Jarvis <danielf.jarvis@staples.com>
2021-01-20 21:19:40 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/Staples-IDaaS-DFJ/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name =  "myresourcegroup"
+  resource_group_name =  azurerm_resource_group.myresourcegroup.name
 }
